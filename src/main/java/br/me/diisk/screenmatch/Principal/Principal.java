5dfba70a63b53a7405ce11ba5defa1e37c23091e
DiisK <diisk@hotmail.com>
2024-03-17 18:07:18 -0300

    .

--- src/main/java/br/me/diisk/screenmatch/Principal/Principal.java
+++ src/main/java/br/me/diisk/screenmatch/Principal/Principal.java
@@ -1,15 +1,17 @@
 package br.me.diisk.screenmatch.Principal;
 
+import br.me.diisk.screenmatch.model.DadosEpisodio;
 import br.me.diisk.screenmatch.model.DadosSerie;
 import br.me.diisk.screenmatch.model.DadosTemporada;
+import br.me.diisk.screenmatch.model.Episodio;
 import br.me.diisk.screenmatch.service.ConsumoAPI;
 import br.me.diisk.screenmatch.service.ConverteDados;
 
+import javax.swing.text.html.Option;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -31,8 +33,47 @@ public class Principal {
             json = consumoAPI.obterDados(getURL(nome,i));
             DadosTemporada dadosTemporada = conversor.obterDados(json,DadosTemporada.class);
             temporadas.add(dadosTemporada);
-            System.out.println("Temporada "+dadosTemporada.numero()+" - "+dadosTemporada.episodios().size()+" ep(s)");
+            System.out.println("----- Temporada "+dadosTemporada.numero()+" - "+dadosTemporada.episodios().size()+" ep(s) -----");
             dadosTemporada.episodios().forEach(ep-> System.out.println(ep.titulo()));
+        }
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(
+                        temp->temp.episodios().stream()
+                                .map(dadosEp->new Episodio(temp.numero(),dadosEp))
+                )
+                .toList();
+
+        Map<Integer,Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(ep->ep.getAvaliacao()>0)
+                        .collect(Collectors.groupingBy(
+                                Episodio::getTemporada,
+                                Collectors.averagingDouble(Episodio::getAvaliacao))
+                        );
+
+        System.out.println(">>>>> Avaliações por temporada <<<<<");
+        System.out.println(avaliacoesPorTemporada);
+        DoubleSummaryStatistics statistics = episodios.stream()
+                .filter(ep->ep.getAvaliacao()>0)
+                        .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println(statistics);
+
+        System.out.println(">>>>> Top 10 Eps <<<<<");
+        episodios.stream()
+                .sorted(Comparator.comparing(Episodio::getAvaliacao).reversed())
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("Digite o trecho do titulo de um episodio:");
+        var trecho = scanner.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(ep -> ep.getTitulo().toUpperCase().contains(trecho.toUpperCase()))
+                .findFirst();
+        if(episodioBuscado.isPresent()){
+            System.out.println("----- Episodio encontrado! -----");
+            System.out.println(episodioBuscado.get());
+        }else{
+            System.out.println("----- Episodio não encontrado! -----");
         }
     }
 
